05d325c10768e57697d1be67f40c979d5adec5f6
laxmi <laxmi.sh9@gmail.com>
2024-06-27 18:55:40 +0530

    first cn

---
+++ laxmi.py
@@ -0,0 +1,9 @@
+# Function to add two numbers
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# Example usage:
+num1 = 5
+num2 = 3
+result = add_numbers(num1, num2)
+print(f"The sum of {num1} and {num2} is: {result}")
